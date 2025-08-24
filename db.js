c1c64f677258680355a59a057821b300729b427a
manoj-oz <manojchilukuri99@gmail.com>
2025-08-24 14:33:46 +0530

    Fix PostgreSQL .env and connection config

--- db.js
+++ db.js
@@ -1,16 +1,28 @@
 const { Pool } = require("pg");
 
-const isAzure = process.env.NODE_ENV === "azure";
+let pool;
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  ssl: isAzure
-    ? { rejectUnauthorized: false } // Azure requires SSL
-    : false                         // Local DB doesn't use SSL
-});
+if (process.env.NODE_ENV === "local") {
+  // Local PostgreSQL
+  pool = new Pool({
+    host: process.env.LOCAL_DB_HOST,
+    user: process.env.LOCAL_DB_USER,
+    port: process.env.LOCAL_DB_PORT,
+    database: process.env.LOCAL_DB_NAME,
+    password: process.env.LOCAL_DB_PASSWORD
+  });
+} else {
+  // Azure PostgreSQL
+  pool = new Pool({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    ssl: {
+      rejectUnauthorized: false // required for Azure PostgreSQL
+    }
+  });
+}
 
 module.exports = pool;
