234e470629f12bdb9d8d802ce8d673b0ccdc1942
manoj-oz <manojchilukuri99@gmail.com>
2025-08-24 14:11:42 +0530

    new update

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 const { Pool } = require("pg");
 
+const isAzure = process.env.NODE_ENV === "azure";
+
 const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  ssl: {
-    rejectUnauthorized: false // required for Azure PostgreSQL
-  }
+  ssl: isAzure
+    ? { rejectUnauthorized: false } // Azure requires SSL
+    : false                         // Local DB doesn't use SSL
 });
 
 module.exports = pool;
