c88757578815e6bed696c7f664f1e59f87be579a
manoj-oz <manojchilukuri99@gmail.com>
2025-08-24 12:36:11 +0530

    db.js updated

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-
-// Switch between local and Azure DB
-const isAzure = process.env.NODE_ENV === "azure";
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: isAzure ? process.env.AZURE_DB_HOST : process.env.LOCAL_DB_HOST,
-  user: isAzure ? process.env.AZURE_DB_USER : process.env.LOCAL_DB_USER,
-  password: isAzure ? process.env.AZURE_DB_PASSWORD : process.env.LOCAL_DB_PASSWORD,
-  database: isAzure ? process.env.AZURE_DB_NAME : process.env.LOCAL_DB_NAME,
-  port: isAzure ? process.env.AZURE_DB_PORT : process.env.LOCAL_DB_PORT,
-  ssl: isAzure ? { rejectUnauthorized: false } : false
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  ssl: {
+    rejectUnauthorized: false // required for Azure PostgreSQL
+  }
 });
 
 module.exports = pool;
